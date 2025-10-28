39a8a335555e134ce0bd7681514a9b9815cc7ca4
darktoolz <soma@somahorse.com>
2025-10-28 09:38:51 +0300

    fix delim parsing typo

--- .github/actions/repo-parse/action.sh
+++ .github/actions/repo-parse/action.sh
@@ -11,7 +11,7 @@ url="$(tolower "$1")"
 
 startswith "$url" 'http://' && scheme=http || scheme=https
 
-echo "$url" | sed 's/^\w*\:\/\///' | sed 's/\//\t/' | sed -E 's/[(\#\@])([^\#\@]*)$/\t\2\t\1/' | read domain repo tag delim
+echo "$url" | sed 's/^\w*\:\/\///' | sed 's/\//\t/' | sed -E 's/([\#\@])([^\#\@]*)$/\t\2\t\1/' | read domain repo tag delim
 url="$scheme://$domain/$repo$delim$tag"
 
 output scheme domain repo url tag
