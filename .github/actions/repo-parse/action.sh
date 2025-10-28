c1fc5743a45bab30d014c2ce7c5cff516b0ce060
darktoolz <soma@somahorse.com>
2025-10-28 10:17:19 +0300

    simpler run.sh

--- .github/actions/repo-parse/action.sh
+++ .github/actions/repo-parse/action.sh
@@ -5,10 +5,7 @@
 echo "started action:\n   repo-parse $@"
 test -n "$1" || die "require repository url"
 
-#defined GITHUB_OUTPUT && test -s $GITHUB_OUTPUT && truncate -s0 $GITHUB_OUTPUT
-
 url="$(tolower "$1")"
-
 startswith "$url" 'http://' && scheme=http || scheme=https
 
 echo "$url" | sed 's/^\w*\:\/\///' | sed 's/\//\t/' | sed -E 's/([\#\@])([^\#\@]*)$/\t\2\t\1/' | read domain repo tag delim
--- run.sh
+++ run.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-env
-
-run() {
-  local action="$1"
-  shift
-#  test -d .github/actions || die "no actions dir in .:\n$(ls .)"
-#  test -d ".github/actions/$action" || die "no actions dir in:\n$(ls .github/actions)"
-  test -d "${GITHUB_ACTION_PATH}" || die "no actions dir in:\n$(ls ${GITHUB_ACTION_PATH})"
-#  test -f ".github/actions/$action/action.sh" || die "no action file in .github/actions/$action:\n$(ls .github/actions/$action)"
-#  source ".github/actions/$action/action.sh"
-  source "${GITHUB_ACTION_PATH}/action.sh"
-  return $?
-}
-
-run "$@"
+test -d "${GITHUB_ACTION_PATH}" || die "no actions dir in:\n$(ls ${GITHUB_ACTION_PATH})"
+test -f "${GITHUB_ACTION_PATH}/action.sh" || die "no action file in ${GITHUB_ACTION_PATH}:\n$(ls ${GITHUB_ACTION_PATH})"
+exec "${GITHUB_ACTION_PATH}/action.sh" "$@"
