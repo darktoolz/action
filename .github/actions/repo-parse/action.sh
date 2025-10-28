1d5ee1d1d39d9e1ced52405f1432a9154cc312a1
darktoolz <soma@somahorse.com>
2025-10-28 09:37:53 +0300

    add delim parsing

--- .github/actions/repo-parse/action.sh
+++ .github/actions/repo-parse/action.sh
@@ -5,13 +5,13 @@
 echo "started action:\n   repo-parse $@"
 test -n "$1" || die "require repository url"
 
-defined GITHUB_OUTPUT && test -s $GITHUB_OUTPUT && truncate -s0 $GITHUB_OUTPUT
+#defined GITHUB_OUTPUT && test -s $GITHUB_OUTPUT && truncate -s0 $GITHUB_OUTPUT
 
 url="$(tolower "$1")"
 
 startswith "$url" 'http://' && scheme=http || scheme=https
 
-echo "$url" | sed 's/^\w*\:\/\///' | sed 's/\//\t/' | sed -E 's/[\#\@]([^\#\@]*)$/\t\1/' | read domain repo tag
-url="$scheme://$domain/$repo$tag"
+echo "$url" | sed 's/^\w*\:\/\///' | sed 's/\//\t/' | sed -E 's/[(\#\@])([^\#\@]*)$/\t\2\t\1/' | read domain repo tag delim
+url="$scheme://$domain/$repo$delim$tag"
 
 output scheme domain repo url tag
